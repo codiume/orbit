c80ac0137db83c55fb343522c208e820b9302877
Mohammed Elhaouari <9967336+mohammed-elhaouari@users.noreply.github.com>
2022-08-25 23:27:33 +0100

    Add jsonld function & schema-dts (#3)
    * Remove keywords to prevent listing by astro
    
    * Add jsonld method

---
+++ src/jsonld.ts
@@ -0,0 +1,85 @@
+/**
+ * Copyright 2021 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import type { Thing, WithContext, Graph } from "schema-dts";
+
+export function JsonLd(item: Graph, space?: string | number);
+export function JsonLd<T extends Thing>(
+  item: WithContext<T>,
+  space?: string | number
+);
+export function JsonLd(
+  item: Graph | WithContext<Thing>,
+  space?: string | number
+): string {
+  return JSON.stringify(item, safeJsonLdReplacer, space);
+}
+
+type JsonValueScalar = string | boolean | number;
+type JsonValue =
+  | JsonValueScalar
+  | Array<JsonValue>
+  | { [key: string]: JsonValue };
+type JsonReplacer = (_: string, value: JsonValue) => JsonValue | undefined;
+
+const ESCAPE_ENTITIES = Object.freeze({
+  "&": "&amp;",
+  "<": "&lt;",
+  ">": "&gt;",
+  '"': "&quot;",
+  "'": "&apos;",
+});
+const ESCAPE_REGEX = new RegExp(
+  `[${Object.keys(ESCAPE_ENTITIES).join("")}]`,
+  "g"
+);
+const ESCAPE_REPLACER = (t: string): string =>
+  ESCAPE_ENTITIES[t as keyof typeof ESCAPE_ENTITIES];
+
+/**
+ * A replacer for JSON.stringify to strip JSON-LD of illegal HTML entities
+ * per https://www.w3.org/TR/json-ld11/#restrictions-for-contents-of-json-ld-script-elements
+ */
+const safeJsonLdReplacer: JsonReplacer = (() => {
+  // Replace per https://www.w3.org/TR/json-ld11/#restrictions-for-contents-of-json-ld-script-elements
+  // Solution from https://stackoverflow.com/a/5499821/864313
+  return (_: string, value: JsonValue): JsonValue | undefined => {
+    switch (typeof value) {
+      case "object":
+        // Omit null values.
+        if (value === null) {
+          return undefined;
+        }
+
+        return value; // JSON.stringify will recursively call replacer.
+      case "number":
+      case "boolean":
+      case "bigint":
+        return value; // These values are not risky.
+      case "string":
+        return value.replace(ESCAPE_REGEX, ESCAPE_REPLACER);
+      default: {
+        // We shouldn't expect other types.
+        isNever(value);
+
+        // JSON.stringify will remove this element.
+        return undefined;
+      }
+    }
+  };
+})();
+
+// Utility: Assert never
+function isNever(_: never): void {}
