d7fd598731f7d82a3acfbd342080d81567907f08
Mohammed Elhaouari <9967336+mohammed-elhaouari@users.noreply.github.com>
2022-08-27 00:52:52 +0100

    Refactor build pipeline (#5)
    * Fix build
    * Remove unused deps

--- scripts/build.js
+++ scripts/build.js
@@ -1,17 +1,14 @@
 #!/usr/bin/env node
 import { build } from "esbuild";
-import { copy } from "esbuild-plugin-copy";
-import { deleteAsync } from "del";
+import { clean } from "esbuild-plugin-clean";
 
 (async () => {
-  await clean("dist");
   await build({
     bundle: false,
-    entryPoints: ["src/index.ts","src/jsonld.ts"],
+    entryPoints: ["src/index.ts", "src/jsonld.ts", "src/Schema.astro"],
     format: "esm",
     minify: false,
     outdir: "dist",
-    outExtension: {},
     platform: "node",
     sourcemap: false,
     sourcesContent: false,
@@ -19,20 +16,13 @@ import { deleteAsync } from "del";
     outExtension: {
       ".js": ".mjs",
     },
+    loader: {
+      ".astro": "copy",
+    },
     plugins: [
-      copy({
-        // this is equal to process.cwd(), which means we use cwd path as base path to resolve `to` path
-        // if not specified, this plugin uses ESBuild.build outdir/outfile options as base path.
-        resolveFrom: "cwd",
-        assets: {
-          from: ["./src/components/*"],
-          to: ["./dist/components/*"],
-        },
+      clean({
+        patterns: ["./dist/*"],
       }),
     ],
   });
-
-  async function clean(outdir) {
-    return deleteAsync([`${outdir}/**`, `!${outdir}/**/*.d.ts`]);
-  }
 })();
