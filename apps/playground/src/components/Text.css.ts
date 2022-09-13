779051b035088263eaa9fe7880f5797128cb776c
Elhaouari Mohammmed <9967336+mohammed-elhaouari@users.noreply.github.com>
2022-09-13 13:05:51 +0100

    format code

--- apps/playground/src/components/Text.css.ts
+++ apps/playground/src/components/Text.css.ts
@@ -1,12 +1,12 @@
-import { style, styleVariants } from "@vanilla-extract/css";
+import { style, styleVariants } from '@vanilla-extract/css';
 
 export const root = style({
-  fontFamily: "Comic Sans Ms",
-  color: "blue",
+  fontFamily: 'Comic Sans Ms',
+  color: 'blue'
 });
 
 export const size = styleVariants({
   small: { fontSize: 16 },
   medium: { fontSize: 24 },
-  large: { fontSize: 32 },
+  large: { fontSize: 32 }
 });
