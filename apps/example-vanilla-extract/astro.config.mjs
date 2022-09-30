50127896497e136b10b2dd2b29ebee8eb5013b0c
Elhaouari Mohammmed <m@codiume.com>
2022-09-30 14:15:56 +0100

    format

--- apps/example-vanilla-extract/astro.config.mjs
+++ apps/example-vanilla-extract/astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from 'astro/config';
 
-import vanillaExtract from "astro-vanilla-extract";
+import vanillaExtract from 'astro-vanilla-extract';
 
 // https://astro.build/config
 export default defineConfig({
