f2cf98987ae4c0aa0891519b3869495f92e61eb8
Daniel Vaca Araujo <vacaaraujo@google.com>
2024-07-31 18:07:04 +0200

    fix: astro-purgecss is modifying files keeping the hash fix #450

--- packages/astro-purgecss/src/index.ts
+++ packages/astro-purgecss/src/index.ts
@@ -1,7 +1,9 @@
 import type { AstroIntegration } from 'astro';
-import { PurgeCSS, type UserDefinedOptions } from 'purgecss';
-import { writeFile } from 'node:fs/promises';
+import { promises as fs } from 'node:fs';
+import { readFile, rename, writeFile } from 'node:fs/promises';
+import path from 'node:path';
 import { fileURLToPath } from 'node:url';
+import { PurgeCSS, type UserDefinedOptions } from 'purgecss';
 
 export interface PurgeCSSOptions extends Partial<UserDefinedOptions> {}
 
@@ -14,6 +16,51 @@ function handleWindowsPath(outputPath: string): string {
   outputPath = outputPath.replaceAll('\\', '/');
 
   return outputPath;
+}
+
+async function getHashFromFile(fileBuffer: BufferSource) {
+  const hashBuffer = await crypto.subtle.digest('SHA-256', fileBuffer);
+  const hashArray = Array.from(new Uint8Array(hashBuffer));
+  const hashHex = hashArray
+    .map((b) => b.toString(16).padStart(2, '0'))
+    .join('');
+  return hashHex.substring(0, 7);
+}
+
+async function replaceStringInFile(
+  filePath: string,
+  searchString: string,
+  replaceString: string
+): Promise<void> {
+  try {
+    const data = await fs.readFile(filePath, 'utf8');
+    if (data.includes(searchString)) {
+      const updatedData = data.split(searchString).join(replaceString);
+      await fs.writeFile(filePath, updatedData, 'utf8');
+    }
+  } catch (error) {
+    console.error(`Error processing file ${filePath}:`, error);
+  }
+}
+
+async function processDirectory(
+  dir: string,
+  searchString: string,
+  replaceString: string
+): Promise<void> {
+  try {
+    const entries = await fs.readdir(dir, { withFileTypes: true });
+    for (const entry of entries) {
+      const fullPath = path.join(dir, entry.name);
+      if (entry.isDirectory()) {
+        await processDirectory(fullPath, searchString, replaceString);
+      } else if (entry.isFile()) {
+        await replaceStringInFile(fullPath, searchString, replaceString);
+      }
+    }
+  } catch (error) {
+    console.error(`Error processing directory ${dir}:`, error);
+  }
 }
 
 export default function (options: PurgeCSSOptions = {}): AstroIntegration {
@@ -30,12 +77,29 @@ export default function (options: PurgeCSSOptions = {}): AstroIntegration {
             `${outDir}/**/*.html`,
             `${outDir}/**/*.js`,
             ...(options.content || [])
-          ],
+          ]
         });
         await Promise.all(
           purged
             .filter(({ file }) => file?.endsWith('.css'))
-            .map(async ({ css, file }) => await writeFile(file!, css))
+            .map(async ({ css, file }) => {
+              if (!file) return;
+              await writeFile(file, css);
+
+              // Get content hash
+              const hash = await getHashFromFile(await readFile(file));
+
+              // Rename file
+              const newPath = file.slice(0, -12) + hash + '.css';
+              await rename(file, newPath);
+
+              // Replace old name references by newPath
+              const oldName = file.split('/').pop();
+              const newName = newPath.split('/').pop();
+              if (oldName && newName) {
+                await processDirectory(outDir + '../', oldName, newName);
+              }
+            })
         );
       }
     }
