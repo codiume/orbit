2487b00a871500d82b7c3157fdbb60275cad9a81
Mohammed Elhaouari <m@codiume.com>
2024-09-08 11:30:35 +0100

    [astro-purgecss] Fix Reached heap limit Allocation failed error (#632)
    * Using generator function to walk ouput dir
    
    * chore
    
    * Using routes to purge files instead of purging all files in out directory
    
    * fix file refs not replaced correctly
    
    * update deps
    
    * update
    
    * format
    
    * Fix astro hashing non astro generated files
    
    * Fix tests
    
    * Fix cleaning paths
    
    * Format
    
    * docs(changeset): Fix Reached heap limit Allocation failed error

--- packages/astro-purgecss/src/index.ts
+++ packages/astro-purgecss/src/index.ts
@@ -1,8 +1,13 @@
 import type { AstroIntegration } from 'astro';
-import { fileURLToPath } from 'node:url';
 import { PurgeCSS, type UserDefinedOptions } from 'purgecss';
 
-import { resolveOutputPath, writeCssFile } from './utils';
+import {
+  cleanPath,
+  headline,
+  replaceValueInFile,
+  success,
+  writeCssFile
+} from './utils';
 
 export interface PurgeCSSOptions extends Partial<UserDefinedOptions> {}
 
@@ -10,8 +15,10 @@ function Plugin(options: PurgeCSSOptions = {}): AstroIntegration {
   return {
     name: 'astro-purgecss',
     hooks: {
-      'astro:build:done': async ({ dir }) => {
-        const outDir = resolveOutputPath(fileURLToPath(dir));
+      'astro:build:done': async ({ dir, routes }) => {
+        headline('Generating purged css files...');
+
+        const outDir = cleanPath(dir);
         const purged = await new PurgeCSS().purge({
           css: [`${outDir}/**/*.css`],
           defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
@@ -22,12 +29,53 @@ function Plugin(options: PurgeCSSOptions = {}): AstroIntegration {
             ...(options.content || [])
           ]
         });
-        await Promise.all(
+
+        const processed = await Promise.all(
           purged
             .filter(({ file }) => file?.endsWith('.css'))
-            .map(
-              async ({ css, file }) => await writeCssFile({ css, file, outDir })
-            )
+            .map(async ({ css, file }) => {
+              const processed = await writeCssFile({ css, file } as {
+                css: string;
+                file: string;
+              });
+              success(processed[1].replace(outDir, ''));
+
+              return processed;
+            })
+        );
+
+        headline('Generating purged html pages...');
+        const pages = routes
+          .filter((route) => {
+            if (
+              route.pathname === undefined ||
+              route.distURL === undefined ||
+              route.type !== 'page'
+            ) {
+              return false;
+            }
+
+            return true;
+          })
+          .map((route) => cleanPath(route.distURL as URL));
+
+        await Promise.all(
+          pages.map(async (page) => {
+            await Promise.all(
+              processed.map(async ([oldFile, newFile]) => {
+                // Replace only if name of the old file
+                // is different from name of the new file (hash changes)
+                if (oldFile !== newFile) {
+                  await replaceValueInFile(
+                    page,
+                    oldFile.replace(outDir, ''),
+                    newFile.replace(outDir, '')
+                  );
+                }
+              })
+            );
+            success(page.replace(outDir, ''));
+          })
         );
       }
     }
